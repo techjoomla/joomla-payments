53b5734c01abf7e59064bcf088ef82ff825704a7
Dipti Jagdale <dipti_j@tekdi.net>
2013-03-06 13:58:47 +0000

    Bug #16564  ccavenue lang file not rendered on payment form soem error in ccavenue plg
    git-svn-id: svn+ssh://172.132.45.1/cpg/trunk@56 c07f7193-1f85-4862-a1a2-2a6da3a91d56

--- ccavenue/ccavenue/tmpl/default.php
+++ ccavenue/ccavenue/tmpl/default.php
@@ -21,7 +21,7 @@ defined('_JEXEC') or die('Restricted access');
 				<input type="hidden" name="billing_cust_state" value="">
 				<input type="hidden" name="billing_cust_city" value="">
 				<input type="hidden" name="billing_zip" value=""> 
-				<input type="hidden" name="billing_cust_tel" value="<?php echo $vars->phone; ?>">
+				<input type="hidden" name="billing_cust_tel" value="<?php if(isset($vars->phone) ) echo $vars->phone; ?>">
 				<input type="hidden" name="delivery_cust_name" value=""> 
 				<input type="hidden" name="delivery_cust_address" value="">
 				<input type="hidden" name="delivery_cust_country" value="">
