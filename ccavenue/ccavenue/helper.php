ca3cadcb54e6a54563f29bf8d30faffd82ecfd93
Aniket Chothe <aniket_c@tekdi.net>
2015-02-02 18:49:50 +0530

    Changes in ccavenue payment plugin related to lof file

--- ccavenue/ccavenue/helper.php
+++ ccavenue/ccavenue/helper.php
@@ -4,10 +4,10 @@
  *  @license    GNU General Public License version 2, or later
  */
 defined( '_JEXEC' ) or die( ';)' );
-	jimport('joomla.html.html');
-	jimport( 'joomla.plugin.helper' );
+jimport('joomla.html.html');
+jimport( 'joomla.plugin.helper' );
 class plgPaymentCcavenueHelper
-{ 	
+{
 	function buildCcavenueUrl($secure = true)
 	{
 		//$url = $this->params->get('sandbox') ? 'test.payu.in/_payment' : 'secure.payu.in/_payment';
@@ -16,36 +16,32 @@ class plgPaymentCcavenueHelper
 		}
 		return $url;
 	}
-	
-	
+
+
 	function Storelog($name,$logdata)
 	{
 		jimport('joomla.error.log');
 		$options = "{DATE}\t{TIME}\t{USER}\t{DESC}";
-		if(JVERSION >='1.6.0')
-			$path=JPATH_SITE.'/plugins/payment/'.$name.'/'.$name.'/';
-		else
-			$path=JPATH_SITE.'/plugins/payment/'.$name.'/';	  
-		$my = JFactory::getUser();     
-	
+
+		$my = JFactory::getUser();
+
 		JLog::addLogger(
 			array(
 				'text_file' => $logdata['JT_CLIENT'].'_'.$name.'.log',
-				'text_entry_format' => $options ,
-				'text_file_path' => $path
+				'text_entry_format' => $options
 			),
 			JLog::INFO,
 			$logdata['JT_CLIENT']
 		);
 
 		$logEntry = new JLogEntry('Transaction added', JLog::INFO, $logdata['JT_CLIENT']);
-		$logEntry->user= $my->name.'('.$my->id.')';
-		$logEntry->desc=json_encode($logdata['raw_data']);
+		$logEntry->user = $my->name.'('.$my->id.')';
+		$logEntry->desc = json_encode($logdata['raw_data']);
 
 		JLog::add($logEntry);
 //		$logs = &JLog::getInstance($logdata['JT_CLIENT'].'_'.$name.'.log',$options,$path);
 //		$logs->addEntry(array('user' => $my->name.'('.$my->id.')','desc'=>json_encode($logdata['raw_data'])));
 
 	}
-		
+
 }
